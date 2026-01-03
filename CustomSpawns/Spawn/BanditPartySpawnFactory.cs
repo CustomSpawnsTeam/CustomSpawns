02f10f9219ec699bb59c5a8ed8299cad1b612c54
JoeFwd <jforward.dev@gmail.com>
2026-01-03 22:20:34 +0100

    fix crash when a bandit party spawns near a settlement

--- CustomSpawns/Spawn/BanditPartySpawnFactory.cs
+++ CustomSpawns/Spawn/BanditPartySpawnFactory.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CustomSpawns.Utils;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Map;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Party.PartyComponents;
 using TaleWorlds.CampaignSystem.Settlements;
@@ -13,7 +16,7 @@ namespace CustomSpawns.Spawn
             PartyTemplateObject templateObject, TextObject partyName)
         {
             MobileParty party = BanditPartyComponent.CreateBanditParty(templateObject.StringId + "_" + 1, clan,
-                spawnedSettlement.Hideout, false, templateObject, spawnedSettlement.Hideout.Settlement.Position);
+                GetHomeHideout(spawnedSettlement, clan), false, templateObject, spawnedSettlement.Position);
             return InitParty(party, partyName ?? clan.Name, spawnedSettlement, clan);
         }
 
@@ -38,6 +41,24 @@ namespace CustomSpawns.Spawn
             mobileParty.SetCustomHomeSettlement(homeSettlement);
 
             return mobileParty;
+        }
+
+        private Hideout? GetHomeHideout(Settlement spawnedSettlement, Clan clan)
+        {
+            if (spawnedSettlement.IsHideout)
+            {
+                return spawnedSettlement.Hideout;
+            }
+            // non-native bandit clans might not have any hideouts on the map
+            // however, it looks like passing a nullable hideout to the bandit party doesn't crash  
+            return GetNearestHideoutFromSettlement(spawnedSettlement, clan);
+        }
+
+        private static Hideout? GetNearestHideoutFromSettlement(Settlement spawnedSettlement, Clan clan)
+        {
+            return CampaignUtils.GetNearestSettlement(
+                Hideout.All.Where(hideout => hideout.MapFaction == clan).Select(hideout => hideout.Settlement).ToList(),
+                new List<IMapPoint>() { spawnedSettlement })?.Hideout;
         }
     }
 }
